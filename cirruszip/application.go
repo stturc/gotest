970ccbd7570f5d89e79417f465fcabb4ea8b3081
Stephen Turcol <stephen.turcol@sas.com>
2024-03-06 01:17:10 -0500

    new

--- cirruszip/application.go
+++ cirruszip/application.go
@@ -12,16 +12,16 @@ func Build() {
 	os.MkdirAll(filepath.Join("./build", "zips"), os.ModePerm)
 
 	// Copy over everything needed from the deployer
-	cmd := exec.Command("cp", "--recursive", "submodules/cirrus-deployer/deploy-solution.sh", "build/zips/.")
+	cmd := exec.Command("cp", "--recursive", "submodules/cirrus-deployer/deploy-solution.sh", "build/zips/.").CombinedOutput()
 	cmd.Run()
 
-	cmd = exec.Command("cp", "--recursive", "submodules/cirrus-deployer/deployer", "build/zips/.")
+	cmd = exec.Command("cp", "--recursive", "submodules/cirrus-deployer/deployer", "build/zips/.").CombinedOutput()
 	cmd.Run()
 
-	cmd = exec.Command("cp", "--recursive", "submodules/cirrus-deployer/tools", "build/zips/.")
+	cmd = exec.Command("cp", "--recursive", "submodules/cirrus-deployer/tools", "build/zips/.").CombinedOutput()
 	cmd.Run()
 
-	cmd = exec.Command("cp", "--recursive", "run-manifest.json", "build/zips/.")
+	cmd = exec.Command("cp", "--recursive", "run-manifest.json", "build/zips/.").CombinedOutput()
 	cmd.Run()
 
     // Remove unneeded folders & files from the deployer
@@ -38,27 +38,30 @@ func Build() {
 	// the CirrusMRM.zip file contains everything that needs to be imported to builder-api
 	// namely CirrusMRMApp.zip which contains the ui json files and CirrusMRMLib.zip which
 	// contains the library js and json files
-	cmd = exec.Command("cp", "--recursive", "libs/dist/CirrusMRM.zip", "build/zips/libs/CirrusMRM.zip")
+	cmd = exec.Command("cp", "--recursive", "libs/dist/CirrusMRM.zip", "build/zips/libs/CirrusMRM.zip").CombinedOutput()
 	cmd.Run()
 
     // Create The build/zips/libs path
 	os.MkdirAll(filepath.Join("./build/zips", "cirrus-mrm-app"), os.ModePerm)
 
-	cmd = exec.Command("zip", "-r", "build/temp.zip", "*", "-x 'submodules/*' -x 'saspackage/*' -x '*.git*' -x 'temp/*' -x 'ci/*' -x 'home/*' -x 'dist/*' -x 'libs/*' -x 'build/*' -x 'bd-scan*' -x 'go*' -x 'sage*'")
+	cmd = exec.Command("zip", "-r", "build/temp.zip", "*", "-x 'submodules/*' -x 'saspackage/*' -x '*.git*' -x 'temp/*' -x 'ci/*' -x 'home/*' -x 'dist/*' -x 'libs/*' -x 'build/*' -x 'bd-scan*' -x 'go*' -x 'sage*'").CombinedOutput()
 	cmd.Run()
 
-	cmd = exec.Command("unzip", "build/temp.zip", "-d", "build/zips/cirrus-mrm-app")
+	cmd = exec.Command("unzip", "build/temp.zip", "-d", "build/zips/cirrus-mrm-app").CombinedOutput()
 	cmd.Run()
 
 
-	cmd = exec.Command("zip", "-r", "CirrusMRM.zip", "*")
+	cmd = exec.Command("zip", "-r", "CirrusMRM.zip", "*").CombinedOutput()
 	cmd.Dir = "build/zips"
 	cmd.Run()
 
     // Create The build/zips/libs path
 	os.MkdirAll(filepath.Join(".", "ui"), os.ModePerm)
 	os.MkdirAll(filepath.Join("./ui", "build"), os.ModePerm)
+	
+	cmd = exec.Command("ls", "--R", "build/zips").CombinedOutput()
+	cmd.Run()
 
-	cmd = exec.Command("cp", "--recursive", "build/zips/CirrusMRM.zip", "ui/build/app-ui.zip")
+	cmd = exec.Command("cp", "--recursive", "build/zips/CirrusMRM.zip", "ui/build/app-ui.zip").CombinedOutput()
 	cmd.Run()
 }
